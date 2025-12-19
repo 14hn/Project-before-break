b94e2a402fa312b969c99daed200ed3c7b850fec
14hn <tymellis207@gmail.com>
2025-12-18 20:48:15 -0500

    setting up jframe for control

---
+++ src/Neighborhood.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Neighborhood {
+
+    public Neighborhood(){
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setFocusable(true);
+
+        JPanel panel = new JPanel();
+
+    }
+    public static void main(String[] args) throws Exception {
+        new Neighborhood();
+
+    }
+}
