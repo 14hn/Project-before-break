4f8e42dea45c4781e231cb8d08aeaed500b28ead
14hn <tymellis207@gmail.com>
2026-01-21 12:32:58 -0500

    player changes

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,20 @@
-import java.util.Arrays;
 import java.util.ArrayList;
+
+
 public class Player extends Character{
+    public static ArrayList<ArrayList<Locations>> playerTheNeighborhood; 
+    public static ArrayList<Locations> playerStreet;
+
     public Player(){
 
+       
+
+
+
     }
-    public void movePlayer(String direction){
-    
+public static void main(String[] args) {
+        Player bob = new Player();
+        Locations emptySpace = new Locations();
+        Main.arrayBuilder(playerStreet, playerTheNeighborhood, emptySpace, 7, 7);
+}       
     }
-}
