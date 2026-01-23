4c0332455c29235b7c1191f9737b841a715b7558
14hn <tymellis207@gmail.com>
2026-01-23 12:03:19 -0500

    idk

--- src/House.java
+++ src/House.java
@@ -4,5 +4,7 @@ public class House extends Locations {
 
     public static ArrayList<ArrayList<Locations>> HousetheNeighborhood; 
     public static ArrayList<Locations> Housestreet;
+    private int xLoc = 0;
+    private int yLoc = 0;
 
 }
--- src/Npc.java
+++ src/Npc.java
@@ -4,14 +4,25 @@ public class Npc extends Character {
 
     public static ArrayList<ArrayList<Locations>> npcTheNeighborhood; 
     public static ArrayList<Locations> npcStreet;
+    private int xLoc = 0;
+    private int yLoc = 0;
     
-    public Npc(){
-
+    public Npc(int xLoc, int yLoc){
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
     }
+    public Npc(){
+        this(0,0);
+    }
+
+    public int getXLoc(){
+        return xLoc;
+    }
+    public int getYLoc(){
+        return yLoc; 
+    }
+
     public static void main(String[] args) {
-        ArrayList<Npc> npcStreet = new ArrayList<>();
-        ArrayList<ArrayList<Npc>> npcTheNeighborhood = new ArrayList<>();
-        new Npc();
         Locations emptySpace = new Locations();
         Main.arrayBuilder(npcStreet, npcTheNeighborhood, emptySpace, 7, 7);
     }
